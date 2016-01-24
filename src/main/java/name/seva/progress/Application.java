15ea743572ce21fdc487e4cf75cf6634675221bb
Sergiy Zaychenko <serg.zaychenko@gmail.com>
2016-01-24 15:29:09 +0200

    first commit

---
+++ src/main/java/name/seva/progress/Application.java
@@ -0,0 +1,12 @@
+package name.seva.progress;
+
+/**
+ * @author Sergiy Zaychenko
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("it works!");
+    }
+
+}
