dffd6d37e3405f38b3c3c160b29c8f9d78bc4ed3
seva <serg.zaychenko@gmail.com>
2016-02-04 14:22:23 +0200

    1

---
+++ src/main/java/name/seva/progress/Activity.java
@@ -0,0 +1,17 @@
+package name.seva.progress;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+/**
+ * Created by seva on 03.02.16.
+ *
+ */
+
+    public class Activity {
+
+        private final List<HistoryEnter> history = new ArrayList<>();
+
+    }
+
